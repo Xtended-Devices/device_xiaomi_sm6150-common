1aa4250fadcb1a6fe4110b947f602b5f350538cc
xSylla <syllamodder@gmail.com>
2022-10-09 06:58:10 +0000

    sm6150-common: Enforce fstab suffix to qcom
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,6 +100,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 KERNEL_LD := LD=ld.lld
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 
 # TARGET_KERNEL_APPEND_DTB handling
 ifeq ($(strip $(PRODUCT_USE_DYNAMIC_PARTITIONS)),true)
